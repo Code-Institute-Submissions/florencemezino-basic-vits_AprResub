35265a619db7e9df93b5be90103355162d9fca39
Florence Mezino <florence.mezino@outlook.com>
2022-02-13 20:01:25 +0000

    Added category, health goal and package filtering

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from .models import Product
 from django.contrib import messages
 from django.db.models import Q
+from .models import Product, Category, HealthGoal, Package
 from taggit.models import Tag
 
 # Create your views here.
@@ -11,20 +11,42 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
+    healthgoals = None
+    packages = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'healthgoal' in request.GET:
+            healthgoals =  request.GET['healthgoal'].split(',')
+            products = products.filter(healthgoal__name__in=healthgoals)
+            healthgoals = HealthGoal.objects.filter(name__in=healthgoals)
+
+        if 'package' in request.GET:
+            packages =  request.GET['package'].split(',')
+            products = products.filter(package__name__in=packages)
+            packages = Package.objects.filter(name__in=packages)
+
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.errir(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name_icontains=query) | Q(description_benefits_icontains=query) | Q(tag_icontains=query)
+            queries = Q(name__icontains=query) | Q(description_benefits__icontains=query) | Q(tag__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search_term':query,
+        'current_categories': categories,
+        'current_healthgoals': healthgoals,
+        'current_packages': packages,
     }
 
     return render(request, 'products/products.html', context)
