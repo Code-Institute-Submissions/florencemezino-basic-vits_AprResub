8b0de31648321fad41c71a4cc10a1b43f9ab7f9a
Florence Mezino <florence.mezino@outlook.com>
2022-02-23 22:23:30 +0000

    Deploy database to heroku

--- basic_vits/settings.py
+++ basic_vits/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +128,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
