4d017c094f0f16efa19ba6d615b71f9c42356952
Florence Mezino <florence.mezino@outlook.com>
2022-02-25 13:00:19 +0000

    Added crispy form and authentification login to contact form

--- contact/models.py
+++ contact/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Contact(models.Model):
-    email = models.EmailField()
-    subject = models.CharField(max_length=255)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    subject = models.CharField(max_length=255, null=False, blank=False)
     message = models.TextField()
 
     def __str__(self):
