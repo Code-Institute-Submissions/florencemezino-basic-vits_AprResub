9550e2315e6e07f40c553fb08ff130d3a59372c7
Florence Mezino <florence.mezino@outlook.com>
2022-04-11 03:07:42 +0000

    Fixing contact form in help center

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-from django.template.loader import render_to_string
+# from django.template.loader import render_to_string
 from django.conf import settings
 
 from .forms import ContactForm
 
 
 def contact_view(request):
+    """Send an email to admin with summary of user request """
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -22,14 +23,14 @@ def contact_view(request):
     return render(request, 'contact/contact.html', context)
 
 
-def _send_customer_email(request):
-    """Send the user a summary of their request from contact form by email"""
-    cust_email = request.email
-    subject = render_to_string(
-        'contact/customer_emails/customer_email_subject.txt',
-        )
-    body = render_to_string(
-        'contact/customer_emails/customer_email_body.txt',
-        {'contact_email': settings.DEFAULT_FROM_EMAIL})
+# def _send_customer_email(request):
+#     """Send the user a summary of their request from contact form by email"""
+#     cust_email = request.email
+#     subject = render_to_string(
+#         'contact/customer_emails/customer_email_subject.txt',
+#         )
+#     body = render_to_string(
+#         'contact/customer_emails/customer_email_body.txt',
+#         {'contact_email': settings.DEFAULT_FROM_EMAIL})
     
-    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+#     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
