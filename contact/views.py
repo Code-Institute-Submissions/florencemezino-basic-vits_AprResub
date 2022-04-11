4bc42f3ac1acbad5a4308b7bd6c3f51d11f315bf
Florence Mezino <florence.mezino@outlook.com>
2022-04-11 05:45:55 +0000

    Fixing sending contact form email to user and admin

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-# from django.template.loader import render_to_string
+from django.template.loader import render_to_string
 from django.conf import settings
 
 from .forms import ContactForm
 
 
 def contact_view(request):
-    """Send an email to admin with summary of user request """
+    """Send an email to user and admin with a summary of user request """
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -16,21 +16,21 @@ def contact_view(request):
             email = form.cleaned_data.get('email')
             subject = form.cleaned_data.get('subject')
             message = form.cleaned_data.get('message')
-            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email, settings.DEFAULT_FROM_EMAIL])
             return render(request, 'contact/contact_success.html')
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
 
 
-# def _send_customer_email(request):
-#     """Send the user a summary of their request from contact form by email"""
-#     cust_email = request.email
-#     subject = render_to_string(
-#         'contact/customer_emails/customer_email_subject.txt',
-#         )
-#     body = render_to_string(
-#         'contact/customer_emails/customer_email_body.txt',
-#         {'contact_email': settings.DEFAULT_FROM_EMAIL})
+def _send_customer_email(request):
+    """Send the user a summary of their request from contact form by email"""
+    cust_email = request.email
+    subject = render_to_string(
+        'contact/customer_emails/customer_email_subject.txt',
+        )
+    body = render_to_string(
+        'contact/customer_emails/customer_email_body.txt',
+        {'contact_email': settings.DEFAULT_FROM_EMAIL})
     
-#     send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
