17c2935be2819f3e942f529975fe86ac1f4a4ebb
Florence Mezino <florence.mezino@outlook.com>
2022-04-11 00:30:44 +0000

    Fixing contact form on heroku

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.conf import settings
 
 from .forms import ContactForm
