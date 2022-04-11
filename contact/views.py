6289b706fe605570ccf046720312d904b6755ecf
Florence Mezino <florence.mezino@outlook.com>
2022-04-11 04:32:54 +0000

    Fixing contact form

--- contact/views.py
+++ contact/views.py
@@ -16,7 +16,7 @@ def contact_view(request):
             email = form.cleaned_data.get('email')
             subject = form.cleaned_data.get('subject')
             message = form.cleaned_data.get('message')
-            send_mail(subject, message, [settings.DEFAULT_FROM_EMAIL], [email])
+            send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email])
             return render(request, 'contact/contact_success.html')
     form = ContactForm()
     context = {'form': form}
