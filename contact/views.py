04c7be7a63d6a198247bfec9c9f2b6deeda416b3
Florence Mezino <florence.mezino@outlook.com>
2022-04-11 07:12:07 +0000

    Fixing sending templates with contact form

--- contact/views.py
+++ contact/views.py
@@ -16,21 +16,14 @@ def contact_view(request):
             email = form.cleaned_data.get('email')
             subject = form.cleaned_data.get('subject')
             message = form.cleaned_data.get('message')
+            email_subject = render_to_string(
+                    'contact/customer_emails/customer_email_subject.txt',
+                    )
+            email_message = render_to_string(
+                'contact/customer_emails/customer_email_body.txt', {
+                    'contact_email': settings.DEFAULT_FROM_EMAIL})
             send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [email, settings.DEFAULT_FROM_EMAIL])
             return render(request, 'contact/contact_success.html')
     form = ContactForm()
     context = {'form': form}
     return render(request, 'contact/contact.html', context)
-
-
-def _send_customer_email(request):
-    """Send the user a summary of their request from contact form by email"""
-    cust_email = request.email
-    subject = render_to_string(
-        'contact/customer_emails/customer_email_subject.txt',
-        )
-    body = render_to_string(
-        'contact/customer_emails/customer_email_body.txt',
-        {'contact_email': settings.DEFAULT_FROM_EMAIL})
-    
-    send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
